2df20dc6bf6229f542b9cb34b1b3692f52e91295
Diya Vinod <dhiyavinodarrived@gmail.com>
2026-03-15 19:54:55 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || '"https://vaidyaai-backend.onrender.com"'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://vaidyaai-backend.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
